be3785b7da8fa2301161d567ce796a3d427a4f19
Raam Noble <ra4m@outlook.com>
2018-04-28 15:40:39 -0700

    Setup editor to update title/body

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NoteList from './NoteList';
 import PrivateHeader from './PrivateHeader.js';
+import Editor from './Editor';
 
 const Dashboard = () => {
   return (
@@ -8,6 +9,7 @@ const Dashboard = () => {
       <PrivateHeader title="Dashboard" />
       <div className="page-content">
         <NoteList />
+        <Editor />
       </div>
     </div>
   );
